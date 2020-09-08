ec7354dde41ad416f837284961292c054c1ccbab
naderbanna <60915152+naderbanna@users.noreply.github.com>
2020-09-08 11:13:19 -0700

    Add files via upload

---
+++ helloWorld/main.cpp
@@ -0,0 +1,14 @@
+//main file, where the main method lives
+
+#include <iostream>
+//#include "quiz1.h"
+
+using namespace std;
+
+int main()
+{
+
+  cout << "Hello World!" << endl;
+
+  return 0;
+}
